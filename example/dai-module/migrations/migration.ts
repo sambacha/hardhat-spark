209358f0334d999630892fa6d86b3d2c1f556155
Filip Petrovic <filip.petroviccc@gmail.com>
2020-12-17 14:21:09 +0100

    added example that utilizes dai stable coin

--- example/dai-module/migrations/migration.ts
+++ example/dai-module/migrations/migration.ts
@@ -4,17 +4,12 @@ import {DaiModule} from "./dai_migration";
 
 export const DaiExampleModule = module("DaiExampleModule", async (m: ModuleBuilder) => {
   const module = await DaiModule
-  const resolver = module.getRegistry()
-  if (resolver != null) {
-    module.setResolver(resolver)
-  }
-
   await m.bindModule(module)
 
   const Dai = m.getBinding('Dai')
   const Example = m.contract("Example", Dai)
 
-  Example.afterDeployment(async (AddressProvider: Binding, ...bindings: DeployedContractBinding[]): Promise<DeployedContractBinding[]> => {
+  Example.afterDeployment(m, "firstAfterDeployment", async (AddressProvider: Binding, ...bindings: DeployedContractBinding[]): Promise<void> => {
     const [Example] = bindings
 
     const example = Example.instance()
@@ -22,7 +17,5 @@ export const DaiExampleModule = module("DaiExampleModule", async (m: ModuleBuild
     const daiAddress = await example.getDai()
 
     console.log("THIS IS DAI ADDRESS: ", daiAddress)
-
-    return bindings
   }, Example)
 })
