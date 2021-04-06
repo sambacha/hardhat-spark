544ca4168938d91e7309c6446749dbe9430327d3
Filip Petrovic <filip.petroviccc@gmail.com>
2021-04-06 22:22:22 +0200

    removed typescript checker.

--- src/packages/utils/typescript-checker.ts
+++ src/packages/utils/typescript-checker.ts
@@ -36,11 +36,6 @@ export function isTypescriptSupported() {
 }
 
 export function loadTsNode(test?: boolean) {
-  const hostNode = Boolean(typeof process == 'object' && process.versions && process.versions.node && process.versions.v8);
-  if (hostNode) {
-    return;
-  }
-
   try {
     require.resolve('typescript');
   } catch (error) {
