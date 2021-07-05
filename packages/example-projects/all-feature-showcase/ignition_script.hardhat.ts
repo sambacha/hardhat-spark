84994d119b8fff85953962c62da0dfeee7233d7a
Filip Petrovic <filip.petroviccc@gmail.com>
2021-07-05 13:43:43 +0200

    [example-projects] removed init

--- packages/example-projects/all-feature-showcase/ignition_script.hardhat.ts
+++ packages/example-projects/all-feature-showcase/ignition_script.hardhat.ts
@@ -7,8 +7,6 @@ const networkName = "local";
 async function main() {
   // @ts-ignore
   const ign = hre.ignition;
-  await ign.init(false, false);
-
   await ign.deploy(await ExampleModule, networkName, true);
 }
 
