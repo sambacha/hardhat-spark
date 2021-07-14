45645ea35cb491e63d01bd68b729402806d64018
Filip Petrovic <filip.petroviccc@gmail.com>
2021-07-14 14:39:25 +0200

    [example-projects/basic] implemented a basic test to showcase how user would approach testing with ignition

--- packages/example-projects/all-feature-showcase/ignition_script.hardhat.ts
+++ packages/example-projects/all-feature-showcase/ignition_script.hardhat.ts
@@ -7,9 +7,9 @@ const networkName = "local";
 async function main() {
   // @ts-ignore
   const ign = hre.ignition;
-  await ign.init(true, false);
+  await ign.init(false, false);
 
-  await ign.deploy(await ExampleModule, networkName);
+  await ign.deploy(await ExampleModule, networkName, true);
 }
 
 main()
