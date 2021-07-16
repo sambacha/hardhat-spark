f0df0a2f462e88ded6ce42e6dfd8252f31d2a155
Filip Petrovic <filip.petroviccc@gmail.com>
2021-07-16 12:01:43 +0200

    [example-project/tornado_core] fixed custom compilation of hasher

---
+++ packages/example-projects/tornado_core/compileHasher.hardhat.ts
@@ -0,0 +1,21 @@
+import { task } from 'hardhat/config';
+const path = require('path')
+const fs = require('fs')
+const genContract = require('circomlib/src/mimcsponge_gencontract.js')
+
+const outputPath = path.join(__dirname, 'artifacts', 'contracts', 'MerkleTreeWithHistory.sol', 'Hasher.json')
+
+task("compile",async (args, hre, runSuper) => {
+  await runSuper();
+
+  const contract = {
+    "_format": "hh-sol-artifact-1",
+    sourceName: "contracts/MerkleTreeWithHistory.sol",
+    deployedBytecode: "0x",
+    contractName: 'Hasher',
+    abi: genContract.abi,
+    bytecode: genContract.createCode('mimcsponge', 220)
+  }
+
+  fs.writeFileSync(outputPath, JSON.stringify(contract, undefined, 4))
+});
